cde3e85bf6942582e58880c9acaa55beb1b8583e
sergio giraldo <sergiorgiraldo@gmail.com>
2018-09-14 21:41:36 -0300

    20180914 night

---
+++ jupytext101/.ipynb_checkpoints/Foo-checkpoint.py
@@ -0,0 +1,36 @@
+# ---
+# jupyter:
+#   jupytext_format_version: '1.2'
+#   jupytext_formats: ipynb,py
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+#   language_info:
+#     codemirror_mode:
+#       name: ipython
+#       version: 3
+#     file_extension: .py
+#     mimetype: text/x-python
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
+#     version: 3.6.5
+# ---
+
+# *foo*
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+print(quicksort([3,6,8,10,1,2,1]))
+
+strs = ['aa', 'BB', 'zz', 'CC']
+print(sorted(strs))
+
