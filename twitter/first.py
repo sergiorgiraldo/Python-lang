b318db6e1546fed7a3c4ca4e1a4cff8a0838cc5d
Sergio Giraldo at PagSeguro <sgiraldo@uolinc.com>
2018-09-27 16:27:50 -0300

    removing credentials

--- twitter/first.py
+++ twitter/first.py
@@ -1,9 +1,9 @@
 import twitter
 
-api = twitter.Api(consumer_key='hk6Ll5M1YSLebZQ2St83sw',
-                      consumer_secret='f3uc8haXnIQ1kLHwpAXieBeEvw6e1i58hRg7jveeU',
-                      access_token_key='8193352-59orz3jZGBnHgB5hQmEQA39nzeznrOrMopASVfgwN9',
-                      access_token_secret='0Lmo8eUlR8yeQfdJKf1SM3uolKm1Y7Ez6LrdY56xS60G6')
+api = twitter.Api(consumer_key='',
+                      consumer_secret='',
+                      access_token_key='',
+                      access_token_secret='')
 
 #todo: iteração
 #todo: guardar último tweet lido
