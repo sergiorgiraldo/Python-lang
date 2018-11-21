0720a615c70f06d7dcfd486537a3c77cbaccccea
Sergio Giraldo at PagSeguro <sgiraldo@uolinc.com>
2018-11-21 16:50:28 -0200

    numpy

---
+++ basics/20181121.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = np.array([1,2,3])
+print("a\n")
+print(a)
+
+b = np.array([(1.5,2,3), (4,5,6)], dtype = ﬂoat)
+print("b\n")
+print(b)
+print(b[1]) // 4 5 6
+
+
+c = np.array([[(1.5,2,3), (4,5,6)], [(3,2,1), (4,5,6)]],dtype = ﬂoat)
+print("c\n")
+print(c)
+
