9aafbbc92f3f234e46b03df7d8a67505d45d5949
sergio giraldo <sergiorgiraldo@gmail.com>
2018-11-27 12:29:37 -0200

    20181127

--- basics/20181127.py
+++ basics/20181127.py
@@ -1,25 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-LInear regression multivariable
-
-Created on Mon Nov 26 22:12:55 2018
+Created on Tue Nov 27 10:58:08 2018
 
 @author: Sergio
 """
 
 import matplotlib.pyplot as plt 
-from sklearn import datasets, linear_model
-
-# load the boston dataset 
-boston = datasets.load_boston(return_X_y=False) 
+import numpy as np
+from sklearn import linear_model
 
 # defining feature matrix(X) and response vector(y) 
-X = boston.data 
-y = boston.target 
+X = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) 
+y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12]) 
 
 # splitting X and y into training and testing sets 
 from sklearn.model_selection import train_test_split 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1) 
+X_train = np.reshape(X_train, (-1, 1))
+X_test = np.reshape(X_test, (-1, 1))
 
 # create linear regression object 
 reg = linear_model.LinearRegression() 
@@ -27,7 +25,11 @@ reg = linear_model.LinearRegression()
 # train the model using the training sets 
 reg.fit(X_train, y_train) 
 
-# regression coefficients 
+# Make predictions using the testing set
+y_pred = reg.predict(X_test)
+
+# regression parameter 
+print('Intercept: \n', reg.intercept_) 
 print('Coefficients: \n', reg.coef_) 
 
 # variance score: 1 means perfect prediction 
@@ -46,6 +48,8 @@ plt.scatter(reg.predict(X_train), reg.predict(X_train) - y_train,
 plt.scatter(reg.predict(X_test), reg.predict(X_test) - y_test, 
 			color = "blue", s = 10, label = 'Test data') 
 
+plt.plot(X_test, y_pred, color='blue', linewidth=3)
+
 ## plotting line for zero residual error 
 plt.hlines(y = 0, xmin = 0, xmax = 50, linewidth = 2) 
 
@@ -57,5 +61,7 @@ plt.title("Residual errors")
 
 ## function to show plot 
 plt.show() 
+
+
 
 
