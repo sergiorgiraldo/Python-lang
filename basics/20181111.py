3e552a80cfe5d88c84ab06f1d34755121c371929
Sergio Giraldo <sergio@giraldo.com.br>
2022-07-04 13:26:35 +0200

    tests

--- basics/20181111.py
+++ basics/20181111.py
@@ -5,8 +5,8 @@ import pandas as pd
 # use `urllib` download files from Earth Lab figshare repository
 
 # download .csv containing monthly average precipitation for Boulder, CO
-urllib.request.urlretrieve(url = "https://ndownloader.figshare.com/files/12710618", 
-                           filename = "avg-precip-months-seasons.csv")
+urllib.request.urlretrieve(url = "https://ndownloader.figshare.com/files/12710618",
+                            filename = "avg-precip-months-seasons.csv")
 
 # print message that data downloads were successful
 print("datasets downloaded successfully")
@@ -17,4 +17,4 @@ avg_precip = pd.read_csv("avg-precip-months-seasons.csv")
 print(type(avg_precip))
 
 # print the values in the pandas dataframe
-avg_precip
+print(avg_precip)
