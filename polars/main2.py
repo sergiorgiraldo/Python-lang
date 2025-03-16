2ba28763f74d11a53ad784c098180529547738d1
Sergio Giraldo <sergio@giraldo.com.br>
2025-03-16 14:54:22 +0100

    feat: testing polars in memory, huge parquet ::by sergio giraldo @ 20250316T1454CET, gpg signed

---
+++ polars/main2.py
@@ -0,0 +1,16 @@
+import polars as pl
+train = pl.read_parquet("train.parquet")
+train2 = pl.read_parquet("train.parquet")
+train3 = pl.read_parquet("train.parquet")
+
+result = (train
+    .join(train2, on="user", how="inner")
+    .join(train3, on="user", how="inner")
+    .select([
+        pl.col("cat_2").alias("train.cat_2"),
+        pl.col("cat_2_right").alias("train2.cat_2"),
+        pl.col("cat_2_right_right").alias("train3.cat_2")
+    ])
+)
+
+print(result)
